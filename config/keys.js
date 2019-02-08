740501c935a1b1073a76fe6cea3f0b61d13702e0
Lee Martin <leemartin@Lees-iMac.local>
2019-02-08 14:51:18 -0500

    Finished Profile routes

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://Lee:crusher79@ds119765.mlab.com:19765/udemymongoreact"
+    mongoURI: "mongodb://Lee:crusher79@ds119765.mlab.com:19765/udemymongoreact",
+    secretOrKey: 'secret'
 }
